3f384c93795cfcdf3445e903f5b919beb0abbec2
bamb1z <michelle.morilhas@gmail.com>
2022-03-16 17:12:35 -0300

    criando a ContaSalario e tornando o método sacar em abstrato

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import { Cliente } from "./Cliente.js";
 import { ContaCorrente } from "./ContaCorrente.js";
 import { ContaPoupanca } from "./ContaPoupanca.js";
+import { ContaSalario } from "./ContaSalario.js";
 
 const cliente1 = new Cliente("Ricardo", 11122233309);
 const contaCorrente1 = new ContaCorrente(cliente1, 1001);
@@ -10,6 +11,12 @@ contaCorrente1.sacar(100);
 
 const contaPoupanca = new ContaPoupanca(50, cliente1, 1001);
 contaPoupanca.sacar(10);
+
+const contaSalario = new ContaSalario(cliente1);
+contaSalario.depositar(200);
+contaSalario.sacar(20);
+
+console.log(contaSalario);
 
 
 console.log(contaCorrente1);
